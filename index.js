e744cef3f10e62017e04954812aa2bb40a5b7f16
massimiliano regis <massimiliano.regis@gmail.com>
2021-08-08 10:18:15 -0700

    add search service

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 var box = new (require("./jukebox"))();
 var app = require("express")();
-app.get("/",(req,res,next)=>{
+const services = require("./util/services");
+services.gateway="http://167.172.198.47:3000"
+
+app.get("/",async (req,res,next)=>{
+    res.json(await box.list())
+})
+app.get("/search",async (req,res,next)=>{
+    var {title}= req.query;    
+    
+    res.json(await box.search(title))
+})
+app.get("/download",async (req,res,next)=>{
+    var {title}= req.query;
+    var list = title.split(",");
+    for(var i in list)
+        await box.download(list[i]);
     res.json({})
 })
 app.get("/add/:code",async (req,res,next)=>{
     var response = await box.download(req.params.code)
     res.json(response)
 })
+
 app.listen(3000)
--- jukebox.js
+++ jukebox.js
@@ -1,8 +1,46 @@
 var path = require("path");
-class JukeBox{
+var fs = require("fs");
+const NodeID3 = require('node-id3')
+const {Document} = require("./util/persistence")
+const yt = require('youtube-search-without-api-key');
+const services = require("./util/services");
 
-    download(code){
-        return new Promise(ok=>{
+class Music extends Document{    
+    toJSONDB(){
+        return {
+            title:this.title,
+            videoId:this.videoId,
+            file:this.file
+        }
+    }
+}
+
+class JukeBox{
+    constructor(){
+        this.test();
+        Music.config("db")
+        
+    }
+    async downloadList(array){
+        for(var i in array)
+            await this.download(array[i]);
+    }
+    async download(name){
+        console.log(name)
+        if(name.match(/^[a-zA-Z0-9-_]{11}$/))
+            await this.downloadByCode(name)
+        else      
+            await this.downloadByName(name)        
+    }
+    async downloadByName(name){            
+        var file = await this.search(name);                    
+        await this.downloadByCode(file.videoId)
+    }
+    async downloadByCode(code){           
+        var file=await Music.get({videoId:code})        
+        if(file)  return;       
+        return new Promise((ok,ko)=>{
+            
             var YoutubeMp3Downloader = require("youtube-mp3-downloader");
             var YD = new YoutubeMp3Downloader({
                 //"ffmpegPath": "/path/to/ffmpeg",        // FFmpeg binary location
@@ -17,18 +55,49 @@ class JukeBox{
             YD.download(code);
 
             YD.on("finished", function(err, data) {
+                Music.new({
+                    title:data.title, 
+                    videoId:data.videoId,
+                    file:`${data.videoTitle}.mp3`})
                 ok(data);
             });
 
             YD.on("error", function(error) {
+                ko(error)
                 console.log(error);
             });
 
             YD.on("progress", function(progress) {
                 console.log(JSON.stringify(progress));
             });
-
+          
         })
+    }
+
+    async list(){
+        return await Music.find();
+    }
+    async search(name){
+        var list = await yt.search(name);
+
+        return list.map(item=>({
+            videoId:item.id.videoId,
+            title:item.title,
+            duration_raw:item.duration_raw,
+            thumb:item.snippet.thumbnails.url,
+            views:item.views,
+            download:services.link(`/add/${item.id.videoId}`)
+        }));
+    }
+    async findBestMatch(name){
+        var list = this.search(name);
+        return list.reduce((prev, current)=>{
+            return (parseInt(prev.views) > parseInt(current.views)) ? prev : current
+        }) ;
+    }
+    async test(){
+        console.log(await this.search("manifesto futurista della nuova umanita"))
+        console.log(await this.search("camera a sud vinicio capossela"))
     }
 }
 module.exports=JukeBox
