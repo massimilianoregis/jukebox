a1175ff7ee1731b5c9a924cf73ad836d5037c260
massimiliano regis <massimiliano.regis@gmail.com>
2022-11-13 22:35:02 -0800

    mp3

--- objects/Jukebox.js
+++ objects/Jukebox.js
@@ -75,7 +75,7 @@ class JukeBox{
             var YoutubeMp3Downloader = require("youtube-mp3-downloader");
             var YD = new YoutubeMp3Downloader({
                 //"ffmpegPath": "/path/to/ffmpeg",        // FFmpeg binary location
-                "outputPath": JukeBox.root,    // Output file location (default: the home directory)
+                "outputPath": Music.root,    // Output file location (default: the home directory)
                 "youtubeVideoQuality": "highestaudio",  // Desired video quality (default: highestaudio)
                 "queueParallelism": 2,                  // Download parallelism (default: 1)
                 "progressTimeout": 2000,                // Interval in ms for the progress reports (default: 1000)
