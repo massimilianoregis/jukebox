d622fb24660622c1acd73ac04a776b3c095f65e8
massimiliano regis <massimiliano.regis@gmail.com>
2022-11-19 14:54:32 -0800

    fix

--- remote/Remote.js
+++ remote/Remote.js
@@ -6,11 +6,6 @@ const {PlayOrPause,VolumeUp,VolumeDown,Mute} = require('./keys');
 class Remote{
     constructor(name,services){
         this.keyboard = new ExclusiveKeyboard(name, true);
-this.keyboard.on('keyup', console.log);
-this.keyboard.on('keydown', console.log);
-this.keyboard.on('keypress', console.log);
-this.keyboard.on('close', console.log);
-this.keyboard.on('error', console.error);
     }
     addKey(cmd){
         cmd.listener(this.keyboard);
