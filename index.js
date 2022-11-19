906e9d0ca3572c5bca8c7f8918e1364310e65c06
massimiliano regis <massimiliano.regis@gmail.com>
2022-11-19 14:02:06 -0800

    remote fix

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ require("./util/hash")
 var path = require("path")
 const services = require("axios").create();
 var express = require("express");
+var port = 3002
 var Remote
 try{    
-    Remote= new require('./remote/remote')('event2').default()
+    Remote= new (require('./remote/Remote'))('event2').default()
+    Remote.services={
+        get:async (value)=>(await services.get(`http://localhost:${port}/${value}`).data
+    };
 }catch(e){console.log(e)}
 
 
@@ -15,9 +19,7 @@ var app = express();
         req.hateous=(value)=>`http://${req.get('host')}${path.resolve(req.originalUrl,value)}`
         req.services={
             get:async (value)=>(await services.get(req.hateous(value))).data
-        }
-        Remote.services=req.services;
-        next();
+        }        
     })
     app.use("/jukebox",require("./service/jukebox"))
     app.use("/",express.static("./ui/build"));
