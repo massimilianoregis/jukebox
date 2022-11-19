1814d58ffb715e4d5204b6532b783112c25dd08f
massimiliano regis <massimiliano.regis@gmail.com>
2022-11-19 14:39:12 -0800

    fix

--- remote/keys/PlayOrPause.js
+++ remote/keys/PlayOrPause.js
@@ -2,6 +2,11 @@ var Command = require("../Command");
 class PlayOrPause extends Command{
     keyId="KEY_PLAYPAUSE"
     endpoint='/jukebox/playorpause'
+
+    listener(keyboard){
+        keyboard.on('keydown', ({keyId})=>{if(keyId==this.keyId) this.press()});        
+    }
+    
 }
 
 module.exports=PlayOrPause
