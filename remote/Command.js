d8690ba2df61b6229e8cb85886c9de7b8a008b56
massimiliano regis <massimiliano.regis@gmail.com>
2022-11-19 14:39:19 -0800

    fix

--- remote/Command.js
+++ remote/Command.js
@@ -2,10 +2,9 @@ class Command{
     keyId="";
     endpoint="";
     listener(keyboard){
-        keyboard.on('keypress', ({keyId})=>{if(keyId==this.keyId) this.press()});
+        keyboard.on('keypress', ({keyId})=>{if(keyId==this.keyId) this.press()});        
     }
     press(){
-        console.log(this.constructor.name)
         require("./Remote").services.get(this.endpoint)
     }
 }
